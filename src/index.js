d849d9d3e1e6434bebb4d13fc401d135575a403b
saumya <2saumyaray@gmail.com>
2017-01-11 13:41:57 +0530

    routes are added

--- src/index.js
+++ src/index.js
@@ -5,8 +5,20 @@ import './index.css';
 //
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
+//
+import { Router, Route, IndexRoute, hashHistory, browserHistory, useRouterHistory } from 'react-router'
 
+// Refer : https://github.com/saumya/ReactRouter-102/blob/master/js/app.js
+// IndexRoute : the dafault route or the default view to render
+var routes = (
+              <Route path="/" component={App}>
+                <IndexRoute component={App} />
+                <Route path="see" component={App} />
+                <Route path="add" component={App} />
+              </Route>
+            );
+//
 ReactDOM.render(
-  <App />,
+  <Router routes={routes} history={hashHistory} />,
   document.getElementById('root')
 );
